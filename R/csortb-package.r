51a8fa511b416267c5cc53ea192fa4b3e27f49be
YAP S S <shyanyap@gmail.com>
2017-11-04 16:56:27 +0000

    First Commit

---
+++ R/csortb-package.r
@@ -0,0 +1,10 @@
+#' csortb.
+#'
+#'  Residential Tenancies Board (RTB) Average Monthly Rent Report by
+#'  Property Type, Location, Number of Bedrooms and Year
+#'  2008 to 2016 hosted by Ireland's Central Statistics Offices
+#'
+#'
+#' @name csortb
+#' @docType package
+NULL
